a21b819786699f0b4aa6a8432b866b918a91ad44
fellipxd <philipokedi@gmail.com>
2025-02-06 23:33:37 +0100

    negative numbers

--- src/utils/mathUtils.js
+++ src/utils/mathUtils.js
@@ -1,40 +1,36 @@
 function isArmstrong(num) {
-  const positiveNum = Math.abs(num);
-  const digits = String(positiveNum).split("");
+  const digits = String(num).split("");
   const power = digits.length;
   const sum = digits.reduce((acc, digit) => acc + Math.pow(Number(digit), power), 0);
-  return sum === positiveNum;
+  return sum === num;
 }
 
 function isPrime(num) {
-  const positiveNum = Math.abs(num);
-  if (positiveNum <= 3) return true;
-  if (positiveNum % 2 === 0 || positiveNum % 3 === 0) return false;
+  if (num <= 3) return true;
+  if (num % 2 === 0 || num % 3 === 0) return false;
 
-  for (let i = 5; i * i <= positiveNum; i += 6) {
-    if (positiveNum % i === 0 || positiveNum % (i + 2) === 0) return false;
+  for (let i = 5; i * i <= num; i += 6) {
+    if (num % i === 0 || num % (i + 2) === 0) return false;
   }
   return true;
 }
 
 function isPerfect(num) {
-  const positiveNum = Math.abs(num);
   let sum = 1;
 
-  for (let i = 2; i * i <= positiveNum; i++) {
-    if (positiveNum % i === 0) {
+  for (let i = 2; i * i <= num; i++) {
+    if (num % i === 0) {
       sum += i;
-      if (i * i !== positiveNum) {
-        sum += positiveNum / i;
+      if (i * i !== num) {
+        sum += num / i;
       }
     }
   }
-  return sum === positiveNum;
+  return sum === num;
 }
 
 function getDigitSum(num) {
-  const positiveNum = Math.abs(num);
-  return String(positiveNum)
+  return String(num)
     .split("")
     .reduce((sum, digit) => sum + Number(digit), 0);
 }
